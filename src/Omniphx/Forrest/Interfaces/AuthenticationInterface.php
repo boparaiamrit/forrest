11d32020a8d18644faeeed33dbb2ff241c63ff8a
Amritpal Singh <boparaiamrit@gmail.com>
2016-07-13 15:32:05 +0530

    Code Refactor and Revert and Username and Password added to UserPasswordInterface

--- src/Omniphx/Forrest/Interfaces/AuthenticationInterface.php
+++ src/Omniphx/Forrest/Interfaces/AuthenticationInterface.php
@@ -4,26 +4,26 @@ namespace Omniphx\Forrest\Interfaces;
 
 interface AuthenticationInterface
 {
-    /**
-     * Begin authentication process.
-     *
-     * @param string $url
-     *
-     * @return mixed
-     */
-    public function authenticate($url);
-
-    /**
-     * Refresh authentication token.
-     *
-     * @return mixed $response
-     */
-    public function refresh();
-
-    /**
-     * Revokes authentication token.
-     *
-     * @return mixed $response
-     */
-    public function revoke();
+	/**
+	 * Begin authentication process.
+	 *
+	 * @param string $url
+	 *
+	 * @return mixed
+	 */
+	public function authenticate($url);
+	
+	/**
+	 * Refresh authentication token.
+	 *
+	 * @return mixed $response
+	 */
+	public function refresh();
+	
+	/**
+	 * Revokes authentication token.
+	 *
+	 * @return mixed $response
+	 */
+	public function revoke();
 }
--- src/Omniphx/Forrest/Interfaces/UserPasswordInterface.php
+++ src/Omniphx/Forrest/Interfaces/UserPasswordInterface.php
@@ -4,4 +4,24 @@ namespace Omniphx\Forrest\Interfaces;
 
 interface UserPasswordInterface extends AuthenticationInterface
 {
+	/**
+	 * Begin authentication process.
+	 *
+	 * @param string $url
+	 * @param null   $username
+	 * @param null   $password
+	 *
+	 * @return mixed
+	 */
+	public function authenticate($url, $username = null, $password = null);
+	
+	/**
+	 * Refresh authentication token.
+	 *
+	 * @param null $username
+	 * @param null $password
+	 *
+	 * @return mixed $response
+	 */
+	public function refresh($username = null, $password = null);
 }
